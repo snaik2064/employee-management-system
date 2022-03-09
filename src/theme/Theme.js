696099e2dffb3c76d95093d0c99879a4b303a2db
Shashank <snaik2064@gmail.com>
2022-03-09 23:56:35 +0530

    [New]: Folder structure created.

---
+++ src/theme/Theme.js
@@ -0,0 +1,11 @@
+const Theme = {
+  white: '#ffffff',
+  red: '#D60000',
+  lightRed: '#fbf2f2',
+  darkRed: '#d63649',
+  pink: '#f6d2c9',
+  lightPink: '#ffded6',
+  yellow: '#f7c137',
+};
+
+export default Theme;
